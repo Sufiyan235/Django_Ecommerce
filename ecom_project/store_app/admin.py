4556fb70c707a9fc356fe1070b591030a397c21e
Sufiyan Shaikh <sufiyanirfan235@gmail.com>
2024-06-08 16:22:17 +0530

    updated

--- ecom_project/store_app/admin.py
+++ ecom_project/store_app/admin.py
@@ -47,3 +47,4 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'product_slug': ('product_name',)}
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductOption)
